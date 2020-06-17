577654bd560a3ca1d5eff69838ef7d5527558646
SilverCrow <SilverCrow@DESKTOP-416QJ6G>
2020-06-16 22:06:05 -0500

    Creacion Form GererarReporte

---
+++ DuFa_express/GenerarReporte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuFa_express
+{
+    public partial class GenerarReporte : Form
+    {
+        public GenerarReporte()
+        {
+            InitializeComponent();
+        }
+    }
+}
