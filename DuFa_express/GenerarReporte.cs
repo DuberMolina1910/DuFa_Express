eed38c61958a95c7a8e4a642908df998c1e8cd79
SilverCrow <SilverCrow@DESKTOP-416QJ6G>
2020-06-19 11:06:17 -0500

    ajuste  query para generar repotes en perfil admin

--- DuFa_express/GenerarReporte.cs
+++ DuFa_express/GenerarReporte.cs
@@ -77,7 +77,7 @@ namespace DuFa_express
 
         private void BtnExlGenRepPerAdm_Click(object sender, EventArgs e)
         {
-            if (CmbBxTipRepGenRepPerAdm.Text == "Consulta Asignacion de Usuarios - Sucursales")
+            if (CmbBxTipRepGenRepPerAdm.Text == "Consulta Usuarios del Sistema")
             {
                 int CellHeader = 2, cellstart =2;
                 SLDocument sl = new SLDocument();
@@ -90,14 +90,9 @@ namespace DuFa_express
                 sl.SetCellValue("H" + CellHeader, "DIRDOMUSU");
                 sl.SetCellValue("I" + CellHeader, "NOMTIPOID");
 
-                
                 SqlConnection Connect = DB_Connection.DBConnection();
-
                 SqlCommand command = new SqlCommand(string.Format("SELECT DESCTIPOPER,NUMIDUSU,NOMUSU,FECHNACUSU,TELUSU,CORREOUSU,DIRDOMUSU,NOMTIPOID FROM TABUSUARIOS,TABTIPOPER, TABTIPOID WHERE TABUSUARIOS.IDTIPOPER = TABTIPOPER.IDTIPOPER AND TABUSUARIOS.IDTIPOID = TABTIPOID.IDTIPOID ORDER BY DESCTIPOPER"), Connect);
                 SqlDataReader Reader = command.ExecuteReader();
-                
-                
-
                 while (Reader.Read())
                 {
                     CellHeader++;
@@ -122,24 +117,105 @@ namespace DuFa_express
                 CellStyle.Border.LeftBorder.Color = System.Drawing.Color.Black;
 
                 sl.AutoFitColumn("B", "I");
-
                 sl.SetCellStyle("B" + cellstart, "I" + CellHeader, CellStyle);
 
-                sl.RenameWorksheet(SLDocument.DefaultFirstSheetName, "DF_REPORT_SYS_USERS");
+                sl.RenameWorksheet(SLDocument.DefaultFirstSheetName, "DF_REPORT_SystemUsers");
                 sl.SaveAs("Report" + CmbBxTipRepGenRepPerAdm.Text + ".xlsx");
                 MessageBox.Show("Reporte Generado a Excel", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 Reader.Close();
                 Connect.Close();
-
             }
-            else if (CmbBxTipRepGenRepPerAdm.Text == "Consulta Usuarios del Sistema")
+            else if (CmbBxTipRepGenRepPerAdm.Text == "Consulta Asignacion de Usuarios - Sucursales")
             {
+                int CellHeader = 2, cellstart = 2;
+                SLDocument sl = new SLDocument();
+                sl.SetCellValue("B" + CellHeader, "NOMCIUDAD");
+                sl.SetCellValue("C" + CellHeader, "NOMSUCURSAL");
+                sl.SetCellValue("D" + CellHeader, "NOMUSU");
+                sl.SetCellValue("E" + CellHeader, "NUMIDUSU");
+                sl.SetCellValue("F" + CellHeader, "DESCTIPOPER");
                 
+
+                SqlConnection Connect = DB_Connection.DBConnection();
+                SqlCommand command = new SqlCommand(string.Format("SELECT TABCIUDADES.NOMCIUDAD,TABSUCURSALES.NOMSUCURSAL,TABUSUARIOS.NOMUSU,TABUSUARIOS.NUMIDUSU,TABTIPOPER.DESCTIPOPER FROM TABCIUDADES, TABSUCURSALES, TABUSUARIOS, TABTIPOPER WHERE TABCIUDADES.IDCIUDAD = TABSUCURSALES.IDCIUDAD AND TABTIPOPER.IDTIPOPER = TABUSUARIOS.IDTIPOPER AND TABSUCURSALES.IDSUCURSAL = TABUSUARIOS.SUCOPERARIOS ORDER BY NOMCIUDAD"), Connect);
+                SqlDataReader Reader = command.ExecuteReader();
+                while (Reader.Read())
+                {
+                    CellHeader++;
+                    sl.SetCellValue("B" + CellHeader, Reader["NOMCIUDAD"].ToString());
+                    sl.SetCellValue("C" + CellHeader, Reader["NOMSUCURSAL"].ToString());
+                    sl.SetCellValue("D" + CellHeader, Reader["NOMUSU"].ToString());
+                    sl.SetCellValue("E" + CellHeader, Reader["NUMIDUSU"].ToString());
+                    sl.SetCellValue("F" + CellHeader, Reader["DESCTIPOPER"].ToString());
+                    
+                }
+
+                SLStyle CellStyle = sl.CreateStyle();
+                CellStyle.Border.TopBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.TopBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.BottomBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.BottomBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.RightBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.RightBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.LeftBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.LeftBorder.Color = System.Drawing.Color.Black;
+
+                sl.AutoFitColumn("B", "F");
+                sl.SetCellStyle("B" + cellstart, "F" + CellHeader, CellStyle);
+
+                sl.RenameWorksheet(SLDocument.DefaultFirstSheetName, "DF_REPORT_SystemUsersperSuc");
+                sl.SaveAs("Report" + CmbBxTipRepGenRepPerAdm.Text + ".xlsx");
+                MessageBox.Show("Reporte Generado a Excel", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reader.Close();
+                Connect.Close();
             }
             else if (CmbBxTipRepGenRepPerAdm.Text == "Consulta de Envios por sucursales")
             {
-                
+                int CellHeader = 2, cellstart = 2;
+                SLDocument sl = new SLDocument();
+                sl.SetCellValue("B" + CellHeader, "NOMCIUDAD");
+                sl.SetCellValue("C" + CellHeader, "NOMSUCURSAL");
+                sl.SetCellValue("D" + CellHeader, "IDENVIOGUIA");
+                sl.SetCellValue("E" + CellHeader, "FECHAENVIO");
+                sl.SetCellValue("F" + CellHeader, "VALORENVIO");
+                sl.SetCellValue("G" + CellHeader, "DESCESTADO");
+               
+                SqlConnection Connect = DB_Connection.DBConnection();
+                SqlCommand command = new SqlCommand(string.Format("SELECT TABCIUDADES.NOMCIUDAD,TABSUCURSALES.NOMSUCURSAL,TABENVIOS.IDENVIOGUIA,TABENVIOS.FECHAENVIO,TABENVIOS.VALORENVIO,TABESTADOS.DESCESTADO FROM TABENVIOS, TABSUCURSALES, TABCIUDADES,TABESTADOS WHERE TABCIUDADES.IDCIUDAD = TABSUCURSALES.IDSUCURSAL AND TABENVIOS.IDSUCORI = TABSUCURSALES.IDSUCURSAL AND TABESTADOS.IDESTADO = TABENVIOS.IDESTADO ORDER BY NOMCIUDAD"), Connect);
+                SqlDataReader Reader = command.ExecuteReader();
+                while (Reader.Read())
+                {
+                    CellHeader++;
+                    sl.SetCellValue("B" + CellHeader, Reader["NOMCIUDAD"].ToString());
+                    sl.SetCellValue("C" + CellHeader, Reader["NOMSUCURSAL"].ToString());
+                    sl.SetCellValue("D" + CellHeader, Reader["IDENVIOGUIA"].ToString());
+                    sl.SetCellValue("E" + CellHeader, Reader["FECHAENVIO"].ToString());
+                    sl.SetCellValue("F" + CellHeader, Reader["VALORENVIO"].ToString());
+                    sl.SetCellValue("G" + CellHeader, Reader["DESCESTADO"].ToString());
+
+                }
+
+                SLStyle CellStyle = sl.CreateStyle();
+                CellStyle.Border.TopBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.TopBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.BottomBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.BottomBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.RightBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.RightBorder.Color = System.Drawing.Color.Black;
+                CellStyle.Border.LeftBorder.BorderStyle = BorderStyleValues.Thin;
+                CellStyle.Border.LeftBorder.Color = System.Drawing.Color.Black;
+
+                sl.AutoFitColumn("B", "G");
+                sl.SetCellStyle("B" + cellstart, "G" + CellHeader, CellStyle);
+
+                sl.RenameWorksheet(SLDocument.DefaultFirstSheetName, "DF_REPORT_EnviosperSuc");
+                sl.SaveAs("Report" + CmbBxTipRepGenRepPerAdm.Text + ".xlsx");
+                MessageBox.Show("Reporte Generado a Excel", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reader.Close();
+                Connect.Close();
             }
 
             
