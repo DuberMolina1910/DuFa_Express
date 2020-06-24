aa34acd11dac0fb38069b7b5015fe7f2abb2061e
Duber Molina <datorres548@misena.edu.co>
2020-06-23 19:20:25 -0500

    Ajustes del boton de anulación

--- DuFa_express/GesEnvios.Designer.cs
+++ DuFa_express/GesEnvios.Designer.cs
@@ -32,10 +32,10 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.txtNumGuia = new System.Windows.Forms.TextBox();
             this.cmbEstadoEnvio = new System.Windows.Forms.ComboBox();
-            this.btnAnularEnvio = new System.Windows.Forms.Button();
             this.btnSolAnulacion = new System.Windows.Forms.Button();
             this.lblMsgError = new System.Windows.Forms.Label();
             this.txtDetCancelacion = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGesEnvios)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +46,7 @@
             this.dgvGesEnvios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvGesEnvios.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgvGesEnvios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvGesEnvios.Location = new System.Drawing.Point(15, 159);
+            this.dgvGesEnvios.Location = new System.Drawing.Point(12, 159);
             this.dgvGesEnvios.Name = "dgvGesEnvios";
             this.dgvGesEnvios.RowHeadersVisible = false;
             this.dgvGesEnvios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
@@ -67,7 +67,7 @@
             // txtNumGuia
             // 
             this.txtNumGuia.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumGuia.Location = new System.Drawing.Point(15, 33);
+            this.txtNumGuia.Location = new System.Drawing.Point(12, 32);
             this.txtNumGuia.Name = "txtNumGuia";
             this.txtNumGuia.Size = new System.Drawing.Size(190, 23);
             this.txtNumGuia.TabIndex = 3;
@@ -79,22 +79,11 @@
             // 
             this.cmbEstadoEnvio.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbEstadoEnvio.FormattingEnabled = true;
-            this.cmbEstadoEnvio.Location = new System.Drawing.Point(15, 74);
+            this.cmbEstadoEnvio.Location = new System.Drawing.Point(12, 74);
             this.cmbEstadoEnvio.Name = "cmbEstadoEnvio";
             this.cmbEstadoEnvio.Size = new System.Drawing.Size(190, 24);
             this.cmbEstadoEnvio.TabIndex = 81;
             this.cmbEstadoEnvio.Text = "*ESTADO ENVÍO";
-            // 
-            // btnAnularEnvio
-            // 
-            this.btnAnularEnvio.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAnularEnvio.Location = new System.Drawing.Point(283, 73);
-            this.btnAnularEnvio.Name = "btnAnularEnvio";
-            this.btnAnularEnvio.Size = new System.Drawing.Size(100, 25);
-            this.btnAnularEnvio.TabIndex = 82;
-            this.btnAnularEnvio.Text = "ANULAR";
-            this.btnAnularEnvio.UseVisualStyleBackColor = true;
-            this.btnAnularEnvio.Click += new System.EventHandler(this.btnAnularEnvio_Click);
             // 
             // btnSolAnulacion
             // 
@@ -123,22 +112,32 @@
             // txtDetCancelacion
             // 
             this.txtDetCancelacion.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDetCancelacion.Location = new System.Drawing.Point(389, 73);
+            this.txtDetCancelacion.Location = new System.Drawing.Point(283, 74);
             this.txtDetCancelacion.Multiline = true;
             this.txtDetCancelacion.Name = "txtDetCancelacion";
             this.txtDetCancelacion.Size = new System.Drawing.Size(299, 77);
             this.txtDetCancelacion.TabIndex = 85;
             this.txtDetCancelacion.Text = "Haga una breve descripción del motivo de cancelación.";
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(283, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 77);
+            this.textBox1.TabIndex = 85;
+            this.textBox1.Text = "Haga una breve descripción del motivo de cancelación.";
+            // 
             // GesEnvios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 490);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtDetCancelacion);
             this.Controls.Add(this.lblMsgError);
             this.Controls.Add(this.btnSolAnulacion);
-            this.Controls.Add(this.btnAnularEnvio);
             this.Controls.Add(this.cmbEstadoEnvio);
             this.Controls.Add(this.txtNumGuia);
             this.Controls.Add(this.btnBuscar);
@@ -159,9 +158,9 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.TextBox txtNumGuia;
         private System.Windows.Forms.ComboBox cmbEstadoEnvio;
-        private System.Windows.Forms.Button btnAnularEnvio;
         private System.Windows.Forms.Button btnSolAnulacion;
         private System.Windows.Forms.Label lblMsgError;
         private System.Windows.Forms.TextBox txtDetCancelacion;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
